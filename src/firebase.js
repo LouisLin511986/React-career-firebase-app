ec3448baa8c08e4928246d982eef7f03df033db0
LouisLin511986 <90818126@gcloud.csu.edu.tw>
2023-10-11 15:21:59 +0800

    2310111521

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import 'firebase/compat/firestore';
 import 'firebase/compat/storage';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyA8KfpOdvBZ9JIS94DRy1JgPdTIOq0iurQ",
-    authDomain: "career-firebase-app-130ad.firebaseapp.com",
-    projectId: "career-firebase-app-130ad",
-    storageBucket: "career-firebase-app-130ad.appspot.com",
-    messagingSenderId: "1015120380496",
-    appId: "1:1015120380496:web:59d88df6bbf0911bab0755"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig)
